595d2ce9b57c9e911c4b44005b1d94489164fd8e
wickerman2019 <sverige@i.ua>
2019-11-28 19:46:59 +0300

    Русификация и правка маршрута

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'books/index'
   resources :books
+  root 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
