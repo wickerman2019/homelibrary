fcd44a53b6c88b4ee21d3b2ad6793ac9ed310484
wickerman2019 <sverige@i.ua>
2019-12-17 03:08:07 +0200

    Добавлено редактирование выдачи книги читателям

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -1,12 +1,26 @@
 class ReadersController < ApplicationController
+  before_action :set_reader, only: [:edit, :update]
     def create
         @book = Book.find(params[:book_id])
         @reader = @book.readers.create(reader_params)
         redirect_to book_path(@book)
       end
     
+    # GET /readers/1/edit
+    def edit  
+      @book = Book.find(@reader.id)      
+    end
+
+    def update
+      @reader.update(reader_params)
+    end
+
       private
         def reader_params
           params.require(:reader).permit(:name, :takedate, :returndate)
+        end
+
+        def set_reader
+          @reader = Reader.find(params[:id])
         end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :books do
     resources :readers
   end
+  resources :readers
   root 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
